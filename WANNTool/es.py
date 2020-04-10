7d47c63ae4406e04f7077e6a7e909f65adb0ec71
joaogui1 <joaoguilhermearujo@gmail.com>
2020-04-10 11:11:09 -0300

    needs testing

--- WANNTool/es.py
+++ WANNTool/es.py
@@ -238,30 +238,23 @@ class MAP_Elites:
                sigma_decay=0.999,     # anneal standard deviation
                sigma_limit=0.01,      # stop annealing if less than this
                popsize=256,           # population size
-               elite_ratio=0.1,       # percentage of the elites
-               forget_best=False,     # forget the historical best elites
                weight_decay=0.01,     # weight decay coefficient
                num_bins = 108*108,    # number of MAP-Elites bins
                bin_x = 2,             # Size of each bin in the x dimension
                bin_y = 2              # Size of each bin in the y dimension
               ):
 
+    self.default = np.zeros(num_params)
     self.num_params = num_params
     self.sigma_init = sigma_init
     self.sigma_decay = sigma_decay
     self.sigma_limit = sigma_limit
     self.popsize = popsize
 
-    self.elite_ratio = elite_ratio
-    self.elite_popsize = int(self.popsize * self.elite_ratio)
-
     self.sigma = self.sigma_init
-    self.elite_params = np.zeros((self.elite_popsize, self.num_params))
-    self.elite_rewards = np.zeros(self.elite_popsize)
     self.best_param = np.zeros(self.num_params)
     self.best_reward = 0
     self.first_iteration = True
-    self.forget_best = forget_best
     self.weight_decay = weight_decay
     self.archive = dict()
 
@@ -279,11 +272,13 @@ class MAP_Elites:
       c[idx] = b[idx]
       return c
     
-    elite_range = range(self.elite_popsize)
+    bins = zip(np.random.randint(low=1, high=109, size=self.popsize),
+               np.random.randint(low=1, high=109, size=self.popsize))
     for i in range(self.popsize):
-      idx_a = np.random.choice(elite_range)
-      idx_b = np.random.choice(elite_range)
-      child_params = mate(self.elite_params[idx_a], self.elite_params[idx_b])
+      idx_a = np.random.choice(bins)
+      idx_b = np.random.choice(bins)
+      child_params = mate(self.archive.get(idx_a, self.default), 
+                          self.archive.get(idx_b, self.default))
       solutions.append(child_params + self.epsilon[i])
 
     solutions = np.array(solutions)
@@ -296,41 +291,35 @@ class MAP_Elites:
     assert(len(reward_table_result[0]) == self.popsize), "Inconsistent reward_table size reported."
 
     reward_table = np.array(reward_table_result[0])
-    
+    behavior_table = np.array(reward_table_result[1])
+
     if self.weight_decay > 0:
       l2_decay = compute_weight_decay(self.weight_decay, self.solutions)
       reward_table += l2_decay
 
-    if self.forget_best or self.first_iteration:
-      reward = reward_table
-      solution = self.solutions
-    else:
-      reward = np.concatenate([reward_table, self.elite_rewards])
-      solution = np.concatenate([self.solutions, self.elite_params])
-
-    idx = np.argsort(reward)[::-1][0:self.elite_popsize]
-
-    self.elite_rewards = reward[idx]
-    self.elite_params = solution[idx]
-
-    self.curr_best_reward = self.elite_rewards[0]
+    for idx, behavior in enumerate(behavior_table):
+      assigned_bin = (behavior[0]//2, behavior[1]//2)
+      if assigned_bin not in self.archive.keys():
+        self.archive[assigned_bin] = (self.solutions[idx], reward_table[idx])
+      elif reward_table[idx] > self.archive[assigned_bin][1]:
+        self.archive[assigned_bin] = (self.solutions[idx], reward_table[idx])
     
+    self.curr_best_reward = np.max(reward_table)
+
     if self.first_iteration or (self.curr_best_reward > self.best_reward):
       self.first_iteration = False
-      self.best_reward = self.elite_rewards[0]
-      self.best_param = np.copy(self.elite_params[0])
+      self.best_reward = self.curr_best_reward
+      pos = np.argmax(reward_table)
+      self.best_param = np.copy(self.solutions[pos])
 
     if (self.sigma > self.sigma_limit):
       self.sigma *= self.sigma_decay
 
   def current_param(self):
-    return self.elite_params[0]
+    return self.archive
 
   def set_mu(self, mu):
     pass
-
-  def best_param(self):
-    return self.best_param
 
   def result(self): # return best params so far, along with historically best reward, curr reward, sigma
     return (self.best_param, self.best_reward, self.curr_best_reward, self.sigma)
